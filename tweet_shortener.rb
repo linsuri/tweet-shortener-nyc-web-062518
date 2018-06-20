625a7459c80479d52d606f8ede65242d2ecf56a9
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 00:35:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,13 @@
 
 def dictionary
   dictionary = {
-    
+    "hello" => 'hi',
+    "to, two, too" => '2' ,
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
   }
 end  
   

4b1ae8baa2c2d2b16fb7891d977567f4c025948a
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 01:05:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,13 +33,11 @@ def bulk_tweet_shortener(tweet)
 end
 
 def selective_tweet_shortener(tweet)
-  
-    if tweet.length <= 140
-      tweet
-    else
-      word_substituter(tweet)
-    end
-   
+  if tweet.length <= 140
+    tweet
+  else
+    word_substituter(tweet)
+  end
 end
 
 def shortened_tweet_truncator(tweet)
